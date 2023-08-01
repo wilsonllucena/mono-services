93eea6a3b05279645177ce85e7b21f43c2bc7581
Wilson Lucena <wilsonllucena@gmail.com>
2023-08-01 01:22:44 -0300

    chore: configuração do libs nestjs/microservices e kafkajs

--- subscriber/src/main.ts
+++ subscriber/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['kafka:9093'],
+      },
+      consumer: {
+        groupId: 'subscriber-consumer',
+      },
+    },
+  });
+  logger.log('Conectando ao microserviço subscriber ');
+  await app.startAllMicroservices();
+  logger.log('Microserviço subscriber conectado');
   await app.listen(3001);
 }
 bootstrap();
