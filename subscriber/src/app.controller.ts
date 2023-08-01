50e341d688e6ff83ccf14f95be66e46c4dba34e8
Wilson Lucena <wilsonllucena@gmail.com>
2023-08-01 01:40:32 -0300

    feat: consumindo mensgaem e persistindo no mongodb

--- subscriber/src/app.controller.ts
+++ subscriber/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { KafkaMessage } from '@nestjs/microservices/external/kafka.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  private readonly logger = new Logger(AppController.name);
+
+  @Post()
+  @MessagePattern('create-subscriber')
+  create(@Payload() message: KafkaMessage) {
+    this.logger.log(`Received message: ${JSON.stringify(message)}`);
+
+    this.appService.execute(message);
   }
 }
--- subscriber/src/app.service.ts
+++ subscriber/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Subscriber } from './models/subscriber.schema';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Subscriber!';
+  constructor(
+    @InjectModel(Subscriber.name)
+    private readonly subscriberModel: Model<Subscriber>,
+  ) {}
+
+  execute(subscriber: any): Promise<Subscriber> {
+    const createdSubscriber = new this.subscriberModel(subscriber);
+    return createdSubscriber.save();
   }
 }
