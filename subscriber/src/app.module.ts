d0e816d5a50c750588496e24742c49c054de4e2e
Wilson Lucena <wilsonllucena@gmail.com>
2023-08-01 01:39:50 -0300

    feat: novo modulo para a conexão com banco de dados e instalacão do mongoose

--- subscriber/src/app.module.ts
+++ subscriber/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Subscriber, SubscriberSchema } from './models/subscriber.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    MongooseModule.forFeature([
+      { name: Subscriber.name, schema: SubscriberSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ subscriber/src/models/subscriber.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type SubscriberDocument = HydratedDocument<Subscriber>;
+
+@Schema()
+export class Subscriber {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+}
+
+export const SubscriberSchema = SchemaFactory.createForClass(Subscriber);
