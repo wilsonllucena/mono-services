e78dfbd67f14e126f7d3af2b03e0b08973325f4f
Wilson Lucena <wilsonllucena@gmail.com>
2023-07-31 21:34:54 -0300

    feat: configuração do kafka e envio de dados para o topico

--- api-gateway/src/api.controller.ts
+++ api-gateway/src/api.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Inject, Post, Res } from '@nestjs/common';
 import { ApiService } from './api.service';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+import { RecordMetadata } from 'kafkajs';
+import { Response } from 'express';
 
-@Controller()
+@Controller('subscriber')
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(
+    private readonly apiService: ApiService,
+    @Inject('KAFKA_PRODUCER') private readonly producer: Producer,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.apiService.getHello();
+  @Post()
+  create(
+    @Body() data: any,
+    @Res() res: Response,
+  ): Promise<Response<RecordMetadata[]>> {
+    const response = this.producer.send({
+      topic: 'create-subscriber',
+      messages: [
+        {
+          key: 'create-subscriber',
+          value: JSON.stringify(data),
+        },
+      ],
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return res.status(201).json(response);
   }
 }
--- api-gateway/src/api.module.ts
+++ api-gateway/src/api.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'api-gateway',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [
+    ApiService,
+    {
+      provide: 'KAFKA_PRODUCER',
+      useFactory: async (kafkaService: ClientKafka) => {
+        return kafkaService.connect();
+      },
+      inject: ['KAFKA_SERVICE'],
+    },
+  ],
 })
 export class ApiModule {}
