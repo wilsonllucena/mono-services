79700fedb99ee8ec72e504cd5b2723512cc25988
Wilson Lucena <wilsonllucena@gmail.com>
2023-08-01 01:17:36 -0300

    feat: inclusão do validation pipe para validar dados de entrada na api

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
